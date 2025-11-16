21821860178e2104cd3ea87a63ce2e95c5b24476
AliClast <82327891+AliClast@users.noreply.github.com>
2025-11-16 16:20:13 +0300

    fix auth pages stylization 16.11.2025

--- src/screens/AuthScreen.jsx
+++ src/screens/AuthScreen.jsx
@@ -2,33 +2,46 @@ import React from "react";
 import "./AuthScreen.css";
 import Logo from "../assets/img/logo-snaptic.png";
 
-export default function AuthScreen(){
-    return(
-        <div className="auth-container">
-            <form className="auth-Form">
-                <div className="auth-logo">
-                    <img src={Logo} alt="" />
-                </div>
-                <div className="auth-inputs">
-                    <label htmlFor="auth-username/">UserName: </label>
-                    <input
-                        type="text"
-                        placeholder="*****"
-                        className="auth-input"
-                        id="auth-username"
-                    /><br/>
-                    <label htmlFor="auth-password/">Password: </label>
-                    <input
-                        type="text"
-                        placeholder="********"
-                        className="auth-input"
-                        id="auth-password"
-                    />
-                </div>
-                <div className="auth-button">
-                    <button type="submit">Confirm</button>
-                </div>
-            </form>
+export default function AuthScreen() {
+  return (
+    <div className="auth-container">
+      <form className="auth-form">
+        <div className="auth-logo">
+          <img src={Logo} alt="App logo" />
         </div>
-    );
+
+        <div className="auth-inputs">
+          <label htmlFor="auth-username" id="label-username">
+            Username:
+          </label>
+          <input
+            type="text"
+            placeholder="Минимум 5 символов"
+            className="auth-input"
+            id="auth-username"
+            name="username"
+            minLength={5}
+            required
+          />
+
+          <label htmlFor="auth-password" id="label-password">
+            Password:
+          </label>
+          <input
+            type="password"
+            placeholder="Минимум 8 символов"
+            className="auth-input"
+            id="auth-password"
+            name="password"
+            minLength={8}
+            required
+          />
+        </div>
+
+        <div className="auth-button">
+          <button type="submit">Confirm</button>
+        </div>
+      </form>
+    </div>
+  );
 }
